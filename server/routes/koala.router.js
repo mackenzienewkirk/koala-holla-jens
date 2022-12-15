0550551242adb4b87fe0435e8579678c8138c55c
Mackenzie Newkirk <mackenzie.joy.newkirk@gmail.com>
2022-12-15 14:20:03 -0600

    created GET route to koalas database

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,12 +1,28 @@
+const { Router } = require('express');
 const express = require('express');
 const koalaRouter = express.Router();
 
 
-
 // DB CONNECTION
 
+const pool = require('../modules/pool.js');
 
 // GET
+
+koala.router.get('/', (req,res) => {
+    let sqlQuery = `
+      SELECT * FROM "koalas" 
+        ORDER BY "name" ASC;
+    `;
+    pool.query(sqlQuery)
+    .then((dbRes) => {
+        res.send(dbRes.rows);
+    })
+    .catch((dbErr) => {
+        console.log('No koalas here', dbErr);
+        res.sendStatus(500);
+    })
+})
 
 
 // POST
